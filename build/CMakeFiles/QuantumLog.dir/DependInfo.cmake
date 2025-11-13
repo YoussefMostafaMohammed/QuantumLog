83890276c0d9f657a6128019f03d217a1c5badff
YoussefMostafaMohammed <youssefmostafa453a@gmail.com>
2025-11-13 05:30:31 +0200

    update cmakelists.txt

---
+++ build/CMakeFiles/QuantumLog.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/youssef/modernC++/QuantumLog/src/ConsoleSinkImpl.cpp" "CMakeFiles/QuantumLog.dir/src/ConsoleSinkImpl.cpp.o" "gcc" "CMakeFiles/QuantumLog.dir/src/ConsoleSinkImpl.cpp.o.d"
+  "/home/youssef/modernC++/QuantumLog/src/FileSinkImpl.cpp" "CMakeFiles/QuantumLog.dir/src/FileSinkImpl.cpp.o" "gcc" "CMakeFiles/QuantumLog.dir/src/FileSinkImpl.cpp.o.d"
+  "/home/youssef/modernC++/QuantumLog/src/LogManager.cpp" "CMakeFiles/QuantumLog.dir/src/LogManager.cpp.o" "gcc" "CMakeFiles/QuantumLog.dir/src/LogManager.cpp.o.d"
+  "/home/youssef/modernC++/QuantumLog/src/LogMessage.cpp" "CMakeFiles/QuantumLog.dir/src/LogMessage.cpp.o" "gcc" "CMakeFiles/QuantumLog.dir/src/LogMessage.cpp.o.d"
+  "/home/youssef/modernC++/QuantumLog/src/main.cpp" "CMakeFiles/QuantumLog.dir/src/main.cpp.o" "gcc" "CMakeFiles/QuantumLog.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/QuantumLog.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuantumLog.dir/src/ConsoleSinkImpl.cpp.o"
+  "CMakeFiles/QuantumLog.dir/src/ConsoleSinkImpl.cpp.o.d"
+  "CMakeFiles/QuantumLog.dir/src/FileSinkImpl.cpp.o"
+  "CMakeFiles/QuantumLog.dir/src/FileSinkImpl.cpp.o.d"
+  "CMakeFiles/QuantumLog.dir/src/LogManager.cpp.o"
+  "CMakeFiles/QuantumLog.dir/src/LogManager.cpp.o.d"
+  "CMakeFiles/QuantumLog.dir/src/LogMessage.cpp.o"
+  "CMakeFiles/QuantumLog.dir/src/LogMessage.cpp.o.d"
+  "CMakeFiles/QuantumLog.dir/src/main.cpp.o"
+  "CMakeFiles/QuantumLog.dir/src/main.cpp.o.d"
+  "QuantumLog"
+  "QuantumLog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuantumLog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
